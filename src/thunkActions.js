bac2a1895fc80b936105160e723e33ebee675cd8
Matthew Kline <matthewwkline@gmail.com>
2024-01-28 15:57:39 -0700

    Fixed bug where api response was incorrectly set to "locations"

--- src/thunkActions.js
+++ src/thunkActions.js
@@ -3,8 +3,8 @@ import {fetchLocationsSuccess} from "./reduxActions";
 export const fetchLocations = () => async(dispatch) => {
     try {
         const response = await fetch('http://localhost:3001/locations');
-        const locations = await response.json();
-        dispatch(fetchLocationsSuccess(locations))
+        const data = await response.json();
+        dispatch(fetchLocationsSuccess(data.locations))
     }
     catch (err) {
         console.log(err);
