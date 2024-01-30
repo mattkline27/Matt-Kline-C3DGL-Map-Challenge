00f08aae4d9fc069950d3275886f5337e9315708
Matthew Kline <matthewwkline@gmail.com>
2024-01-30 00:49:55 -0700

    Remove typo from prod api url

--- src/config.js
+++ src/config.js
@@ -1,1 +1,1 @@
-export const API_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:3001' : 'https://mk-c3d-challenge-fe1081196472.herokuapp.com/';
+export const API_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:3001' : 'https://mk-c3d-challenge-fe1081196472.herokuapp.com';
